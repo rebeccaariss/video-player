7a5066e55f2afd8e0e1046a042fcef04028505d7
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-02-03 13:45:58 -0500

    Initialize script variables, add function and event listeners for play/pause

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
-
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+  }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +38,6 @@
 // Fullscreen ------------------------------- //
 
 
+// Event Listeners:
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay); // allows you to click anywhere on the video to play/pause
